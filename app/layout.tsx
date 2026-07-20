bc4f10b109b5fe8680e0ca8fb3339bf28d70c7e1
growxlabstech <growxlabstech@gmail.com>
2026-07-20 12:48:03 +0530

    revert(theme): restore exact original app/globals.css and app/layout.tsx prior to theme changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -255,7 +255,7 @@ export default async function RootLayout({
         />
         <PHProvider>
           <AuthProvider>
-            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true} disableTransitionOnChange>
               <GlobalBackground />
 
               <ConditionalLayout>
